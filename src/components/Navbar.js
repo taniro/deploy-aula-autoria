8d2cc8216ceb0092c319dafc6b913f0ff97fad4e
taniro <tanirocr@gmail.com>
2021-04-26 13:49:42 -0300

    hello

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
-import $ from "jquery";
-import Popper from "popper.js";
 
 function Navbar() {
 	return (
